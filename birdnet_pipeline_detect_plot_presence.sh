bc33aff8499ab2df51613016f33fdeb2a097c50c
aluo734 <55247353+aluo734@users.noreply.github.com>
2023-03-04 12:54:22 -0500

    Delete birdnet_pipeline_detect_plot_presence.sh

--- birdnet_pipeline_detect_plot_presence.sh
+++
@@ -1,9 +0,0 @@
-python3 mp3_to_wav.py -i input_recordings
-
-mkdir birdnet_out
-python3 BirdNET-Analyzer/analyze.py \
---i input_recordings \
---o birdnet_out \
---slist ../spplist.txt
-
-Rscript plot_spp_presence.R
